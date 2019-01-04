3b900459a22ae2e460b377153b9ca934c46cd5d4
Kelsey Low <kklowlow@gmail.com>
2019-01-04 03:33:32 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,21 +1,9 @@
-=begin
 def prime?(number)
   if number >= 2
-    (2..number - 1).none? do |x|
+    (2..number).none? do |x|
       number % x == 0
     end
   else
     return false
   end
-end
-=end
-
-def prime?(number)
-  if number >= 2  
-    (2..number - 1).each do |x|
-      number % x == 0 
-    end
-  else 
-    return false
-  end 
 end
